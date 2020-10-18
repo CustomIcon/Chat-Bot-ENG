12a1c3e4110dea7052e0a3817c20b2fce29b6bb2
ポキ <aman_a@aol.com>
2020-10-18 19:08:13 +0500

    fits into latest api update

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -3,7 +3,7 @@ import aiohttp
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
         async with ses.post(
-            f'https://heroku.mannu.me/bot?query={query}'
+            f'https://endpoint.mannu.me/?query={query}'
         ) as resp:
             answer = await resp.json()
-            return answer['bot_response']
+            return answer['response']['bot']
