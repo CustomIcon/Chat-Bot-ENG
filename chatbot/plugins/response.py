17e734e3e62d5dd49ea1ffb1c8fb526c8b58ca62
reejit <70316178+reejit@users.noreply.github.com>
2020-11-23 15:54:45 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -5,5 +5,5 @@ async def get_response(query):
         async with ses.post(
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
-            answer = await resp.json()
+            answer = await resp.html()
             return answer['response']
