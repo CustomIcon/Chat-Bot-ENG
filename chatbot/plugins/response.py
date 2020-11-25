064b8b12adeaed88fc2c8403b81b89538ee43695
reejit <70316178+reejit@users.noreply.github.com>
2020-11-25 19:51:19 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,9 +1,14 @@
 import aiohttp
+import asyncio
 
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
-        async with ses.post(
-            f'https://endpoint.mannu.me/?query={query}'
+        async with ses.get(
+            f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
-            answer = await resp.json()
-            return answer['response']['bot']
+            return (await resp.json())['response']
+
+async def main():
+    print(await get_response('world'))
+
+asyncio.run(main())
