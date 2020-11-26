dc97183ef9bbd4b2ffe56c96fb384f43d00ae8d8
reejit <70316178+reejit@users.noreply.github.com>
2020-11-26 12:30:39 +0530

    Update response.py
    No

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,11 +1,8 @@
-import aiohttp
-import asyncio #to run async funtions you need to import asyncio
-
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
         async with ses.get(
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
-            return (await resp.json())['response']
-
-print((asyncio.run( get_response('world')))
+            return (await resp.json())['data']['response']
+async def main():
+    print(await get_response('world'))
