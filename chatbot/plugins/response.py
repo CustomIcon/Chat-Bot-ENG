3f85ca927c92977b06f72be1a6bf64ec774a6b54
reejit <70316178+reejit@users.noreply.github.com>
2020-11-26 18:53:23 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,10 +1,11 @@
 import aiohttp
 import asyncio
+
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
         async with ses.get(
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
             return (await resp.json())['response']
-async def main():
-    print(await get_response('world'))
+def main():
+    print(asyncio.run(get_response('world')))
