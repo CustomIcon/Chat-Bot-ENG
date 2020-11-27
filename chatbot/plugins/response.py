f5685df4dc98d6b95c03c3fdd3e052c9e149a323
reejit <70316178+reejit@users.noreply.github.com>
2020-11-27 15:16:40 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,6 +1,6 @@
 import aiohttp
 import asyncio
-from multithreading import threading
+
 
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
@@ -8,22 +8,7 @@ async def get_response(query):
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
             return (await resp.json())['response']
-def BotThread():
+async def BotThread():
     response = asyncio.run( get_response('world'))
-    print(response)
+    await asyncio.gather(*[get_response for _ in range(500)])
         
-def main():
-    # Create ten threads as follows
-    try:
-        thread.start_new_thread( BotThread, ("Thread-1", 2, ) )
-        thread.start_new_thread( BotThread, ("Thread-2", 4, ) )
-        thread.start_new_thread( BotThread, ("Thread-3", 6, ) )
-        thread.start_new_thread( BotThread, ("Thread-4", 8, ) )
-        thread.start_new_thread( BotThread, ("Thread-5", 10, ) )
-        thread.start_new_thread( BotThread, ("Thread-6", 12, ) )
-        thread.start_new_thread( BotThread, ("Thread-7", 14, ) )
-        thread.start_new_thread( BotThread, ("Thread-8", 16, ) )
-        thread.start_new_thread( BotThread, ("Thread-9", 18, ) )
-        thread.start_new_thread( BotThread, ("Thread-10", 20, ) )
-    except:
-       print("Error: unable to start thread")
