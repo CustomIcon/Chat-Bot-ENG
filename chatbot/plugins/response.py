c49f17583115747932c5bd60d3f22d72565b6e85
reejit <70316178+reejit@users.noreply.github.com>
2020-11-27 14:46:00 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+import thread
 
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
@@ -7,5 +8,22 @@ async def get_response(query):
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
             return (await resp.json())['response']
+def BotThread():
+    response = asyncio.run( get_response('world'))
+    print(response)
+        
 def main():
-    print(asyncio.run(get_response('world')))
+    # Create ten threads as follows
+    try:
+        thread.start_new_thread( BotThread, ("Thread-1", 2, ) )
+        thread.start_new_thread( BotThread, ("Thread-2", 4, ) )
+        thread.start_new_thread( BotThread, ("Thread-3", 6, ) )
+        thread.start_new_thread( BotThread, ("Thread-4", 8, ) )
+        thread.start_new_thread( BotThread, ("Thread-5", 10, ) )
+        thread.start_new_thread( BotThread, ("Thread-6", 12, ) )
+        thread.start_new_thread( BotThread, ("Thread-7", 14, ) )
+        thread.start_new_thread( BotThread, ("Thread-8", 16, ) )
+        thread.start_new_thread( BotThread, ("Thread-9", 18, ) )
+        thread.start_new_thread( BotThread, ("Thread-10", 20, ) )
+    except:
+       print("Error: unable to start thread")
