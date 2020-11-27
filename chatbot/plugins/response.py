632b5e54a2bb69f4c10dc9ece354fac51072ac78
reejit <70316178+reejit@users.noreply.github.com>
2020-11-27 15:27:02 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -8,7 +8,12 @@ async def get_response(query):
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
             return (await resp.json())['response']
-async def BotThread():
-    response = asyncio.run( get_response('world'))
-    await asyncio.gather(*[get_response for _ in range(500)])
-        
+
+loop = asyncio.get_event_loop()
+group1 = asyncio.gather(*[get_response("group 1.{}".format(i)) for i in range(1, 6)])
+group2 = asyncio.gather(*[get_response("group 2.{}".format(i)) for i in range(1, 4)])
+group3 = asyncio.gather(*[get_response("group 3.{}".format(i)) for i in range(1, 10)])
+all_groups = asyncio.gather(group1, group2, group3)
+results = loop.run_until_complete(all_groups)
+loop.close()
+print(results)
