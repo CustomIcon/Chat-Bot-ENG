3aba2a8c28c5a4a6eafae4f7b93cffed7b2911b5
reejit <70316178+reejit@users.noreply.github.com>
2020-11-28 10:01:09 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,5 +1,3 @@
-
-
 import aiohttp
 import asyncio
 
@@ -9,14 +7,13 @@ async def get_response(query):
         async with ses.get(
             f'https://some-random-api.ml/chatbot?message={query}'
         ) as resp:
-            return (await resp.json()),['response']
+            return(await resp.json())['response']
     
 #using an event loop
 loop = asyncio.get_event_loop()
-Task = asyncio.gather(*[get_response('response') for _ in range(800)])
+Task = asyncio.gather(*[get_response('world') for _ in range(500)])
 
 try:
     loop.run_until_complete(Task)
 finally:
-    loop.stop()
     loop.close()
