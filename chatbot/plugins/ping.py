dc865952dadd1c2083cbeea81daf22c762a6b536
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 16:22:32 +0530

    Update ping.py

--- chatbot/plugins/ping.py
+++ chatbot/plugins/ping.py
@@ -13,4 +13,4 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pong!\n{time_taken_s:.3f}ms\nUptime: {start_t:.3f}")
+    await rm.edit(f"Pong!\n{time_taken_s:.3f}ms\nUptime: {time.time() - start_t:.3f}")
