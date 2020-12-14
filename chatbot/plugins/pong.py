8e44c22b8eacc87efc296135199dd148777b60c4
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 15:50:39 +0530

    Update pong.py

--- chatbot/plugins/pong.py
+++ chatbot/plugins/pong.py
@@ -1,5 +1,6 @@
 import aiohttp
-async def ping(query):
+
+async def ping(query):):
     async with aiohttp.ClientSession() as ses:
         async with ses.post(
             f'https://blooming-brook-96225.herokuapp.com/?query=hi'
