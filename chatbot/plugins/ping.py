9005ec7a141bd8aef967f166d049003535246461
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 15:49:55 +0530

    Create ping.py

---
+++ chatbot/plugins/ping.py
@@ -0,0 +1,27 @@
+import asyncio
+from pyrogram import filters
+from chatbot import chatbot
+from chatbot.plugins.pong import ping
+
+
+@chatbot.on_message(Filters.private & Filters.command("start"))
+async def chat_bot(client, message):
+    if message.text:
+        chat_id = message.chat.id
+        if await check_message(client, message):
+            query = message.text
+            await client.send_chat_action(chat_id, action='typing')
+            rep = await get_response(query)
+            await message.reply_text(rep)
+
+
+async def check_message(client, message):
+    if message.chat.type == 'private':
+        return True
+    Bot = await client.get_me()
+    if message.text.lower() == f"@{Bot.username}":
+        return True
+    elif message.reply_to_message:
+        return message.reply_to_message.from_user.id == Bot.id
+    else:
+        return False
