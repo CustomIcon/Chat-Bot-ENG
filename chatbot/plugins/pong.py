684d37601acdb745a08d7e837db09b0d67cfdbb7
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 16:11:39 +0530

    Delete pong.py

--- chatbot/plugins/pong.py
+++
@@ -1,9 +0,0 @@
-import aiohttp
-
-async def ping(query):
-    async with aiohttp.ClientSession() as ses:
-        async with ses.post(
-            f'https://blooming-brook-96225.herokuapp.com/?query=hi'
-        ) as resp:
-            answer = await resp.json()
-            return answer['response']['time_taken']
