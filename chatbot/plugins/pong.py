df377c153ac2f2a5616ff8bb8d5df2fa0e4071e9
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 15:40:45 +0530

    Update pong.py

--- chatbot/plugins/pong.py
+++ chatbot/plugins/pong.py
@@ -1,9 +1,17 @@
 import aiohttp
+from pyrogram import filters as  Filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from ..config import Config
+from ..chatbot import chatbot
+
+
+@chatbot.on_message(Filters.private & Filters.command("ping"))
 
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
         async with ses.post(
-            f'https://blooming-brook-96225.herokuapp.com/?query={query}'
+            f'https://blooming-brook-96225.herokuapp.com/?query=hi'
         ) as resp:
             answer = await resp.json()
             return answer['response']['time_taken']
