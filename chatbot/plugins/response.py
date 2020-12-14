1d17d2662fa7bd9445a43e9c94b712eb8bb86656
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 13:07:59 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -3,7 +3,7 @@ import aiohttp
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
         async with ses.post(
-            f'https://blooming-brook-96225.herokuapp.com/query={query}'
+            f'https://blooming-brook-96225.herokuapp.com/?query={query}'
         ) as resp:
             answer = await resp.json()
             return answer['response']['bot']
