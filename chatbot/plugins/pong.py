f2f9afe6bd40fbb5559740835a254c5941e1a9d6
reejit <70316178+reejit@users.noreply.github.com>
2020-12-14 15:35:37 +0530

    Create pong.py

---
+++ chatbot/plugins/pong.py
@@ -0,0 +1,9 @@
+import aiohttp
+
+async def get_response(query):
+    async with aiohttp.ClientSession() as ses:
+        async with ses.post(
+            f'https://blooming-brook-96225.herokuapp.com/?query={query}'
+        ) as resp:
+            answer = await resp.json()
+            return answer['response']['time_taken']
