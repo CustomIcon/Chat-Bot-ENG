e91695feb27209952b10ddf0d451088f9b332c51
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-20 17:06:19 +0530

    new api

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -1,9 +1,14 @@
-import aiohttp
 
-async def get_response(query):
-    async with aiohttp.ClientSession() as ses:
-        async with ses.post(
-            f'https://endpoint.mannu.me/?query={query}'
-        ) as resp:
-            answer = await resp.json()
-            return answer['response']['bot']
+from requests.exceptions import HTTPError
+import requests
+
+url = "https://api.affiliateplus.xyz/api"
+
+
+def chatbot(msg, ainame, onwer, userid):
+    base = f'{url}/chatbot?message={msg}&botname={ainame}&ownername={owner}&user={userid}'
+    response = requests.get(base)
+    response.raise_for_status()
+    jsonResponse = response.json()
+    message = (jsonResponse["message"])
+    return message
