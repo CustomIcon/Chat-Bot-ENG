05be56ae49b58e0720c4308097ddcd59e73e1348
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-20 17:05:32 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -3,7 +3,7 @@ import aiohttp
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
         async with ses.post(
-            f'https://endpoint.mannu.me/?query={query}'
+            f'http://api.brainshop.ai/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=1&msg={query}'
         ) as resp:
             answer = await resp.json()
-            return answer['response']['bot']
+            return answer['cnt']
